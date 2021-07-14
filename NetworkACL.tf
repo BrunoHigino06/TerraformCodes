bed3576cc8033f8adea0ad262456e03c9b7ba51f
BrunoHigino06 <72587932+BrunoHigino06@users.noreply.github.com>
2021-07-14 16:56:53 -0300

    Add files via upload

---
+++ NetworkACL.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_network_acl" "main" {
+  vpc_id = "vpc-cdadfab5"
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "10.3.0.0/18"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.3.0.0/18"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  tags = {
+    Name = "TesteNET_ACL"
+  }
+}
